dd99dee3eb5dbfe9c6bda8b984f2801e8d85d3e1
Osman Ekrem Korkmaz <korkmazosmanekrem@gmail.com>
2022-07-04 21:25:37 +0300

    files added

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.end("Merhaba Socket.IO");
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+
+	socket.on("new-message", (message) => {
+		console.log(message);
+
+		socket.broadcast.emit("receive-message", message);
+	});
+
+	socket.on("disconnect", () => console.log("a user disconnected"));
+});
+
+http.listen("3000", () => {
+	console.log("listening on *:3000");
+});
