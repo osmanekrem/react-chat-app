a8f1a57a48cf653e2323027fcbd1e73caa542667
Osman Ekrem Korkmaz <korkmazosmanekrem@gmail.com>
2022-07-06 20:56:52 +0300

    send button added

--- client/src/components/CharForm.js
+++ client/src/components/CharForm.js
@@ -18,6 +18,9 @@ function CharForm() {
     <div>
         <form onSubmit={handleSubmit}>
             <input className={styles.textInput} value={message} onChange={(e) => setMessage(e.target.value)} type="text" placeholder="Type a message..." />
+            <button className={styles.sendButton} type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" fill='currentColor' height="48" width="48"><path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z"/></svg>
+            </button>
         </form>
     </div>
   )
